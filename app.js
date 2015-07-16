2c7a020b44e76df111f426ca2c37512682702d24
reydelo <reyna.deloge@gmail.com>
2015-07-16 14:33:46 -0600

    facebook oauth config

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ var routes = require('./routes');
 var path = require('path');
 var mongoose = require('mongoose');
 
+// oath dependencies
+var config = require('./oauth.js')
+var mongoose = require('mongoose')
+var passport = require('passport')
+var FacebookStrategy = require('passport-facebook').Strategy;
+var GoogleStrategy = require('passport-google').Strategy;
+
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+done(null, obj);
+});
+
 // global config
 var app = express();
 app.set('port', process.env.PORT || 1337);
@@ -33,10 +48,68 @@ mongoose.connect(mongo);
 
 // mongo model
 // var Model_Name = require('add_your_models_here');
+// config
+passport.use(new FacebookStrategy({
+ clientID: config.facebook.clientID,
+ clientSecret: config.facebook.clientSecret,
+ callbackURL: config.facebook.callbackURL
+},
+function(accessToken, refreshToken, profile, done) {
+ process.nextTick(function () {
+   return done(null, profile);
+ });
+}
+));
+
+var app = express();
+
+app.configure(function() {
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger());
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'my_precious' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+});
 
 // routes
 app.get('/', routes.index);
 app.get('/ping', routes.ping);
+app.get('/account', ensureAuthenticated, function(req, res){
+res.render('account', { user: req.user });
+});
+
+app.get('/', function(req, res){
+res.render('login', { user: req.user });
+});
+
+app.get('/auth/facebook',
+passport.authenticate('facebook'),
+function(req, res){
+});
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/account');
+});
+app.get('/logout', function(req, res){
+req.logout();
+res.redirect('/');
+});
+
+// port
+app.listen(1337);
+
+// test authentication
+function ensureAuthenticated(req, res, next) {
+if (req.isAuthenticated()) { return next(); }
+res.redirect('/')
+}
 
 // run server
 app.listen(app.get('port'), function(){
